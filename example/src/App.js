2fe3833fccc8b6505b9156b412bc0a318fa8cb59
Mudit Panwar <mudit56@outlook.com>
2020-04-22 00:45:06 +0530

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+import InputDrop from 'react-input-drop';
+import 'react-input-drop/dist/index.css';
 
-import { ExampleComponent } from 'react-input-drop'
-import 'react-input-drop/dist/index.css'
+class App extends React.Component {
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  state = {
+    options : [],
+    selected: null,
+  }
+
+  whenFocusAndChange = async (value = '') => {
+    const response = await fetch(`http://54.200.190.162:4002/api/company/searchCompensationCompany?name=${value}`);
+    let res;
+    let data;
+    if(response.ok) {
+      res = await response.json();
+      data = res.data || [];
+    }
+    this.setState({options: data});
+  }
+
+  onSelectHandler = (selectedOption) => {
+    //
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <InputDrop
+            whenFocusAndChange = {this.whenFocusAndChange}
+            options={this.state.options}
+            optionConfig={["companyName","_id"]}
+            whenSelected={this.onSelectHandler}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App
+export default App;
